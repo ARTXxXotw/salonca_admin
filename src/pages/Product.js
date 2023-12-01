94de1aa264fa4cac9bd298685bb3c50f4c985395
abzal <abdunabiyevotabek7@gmail.com>
2023-12-01 11:07:53 +0200

    a
    sa

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -13,17 +13,20 @@ export default function Product() {
 
     useEffect(()=>{
         axios.get(`https://fre.abbas.uz/api/filyal`).then(res=>{
-            setData(res.data)
-            console.log(res.data)
-        })
+            setData(res.data);
+                  })
     })
+
+    
+
+ 
 
    function modalOpen(id){
     setDataiD(id)
     document.querySelector(".bu-bir").style=`display:block`
    }
 
-   function delet(){
+   function delet(){  
     
     axios.delete(`https://fre.abbas.uz/api/filyal/${dataiD}`).then(res=>{
       alert("Вы удалили этот товар")
@@ -118,6 +121,7 @@ const [mutahasi1,setMutahasi1]=useState([]);
 const [filyal,setFilyal]=useState([]);
 const [filyal1,setFilyal1]=useState([]);
 const [filyal2,setFilyal2]=useState([]);
+const [mark,setMark]=useState([]);
 
 
 useEffect(()=>{
@@ -143,7 +147,6 @@ useEffect(()=>{
 
 axios.get(`https://fre.abbas.uz/api/filyal_image/`).then(res=>{
   setFilyal(res.data)
-
 })
 
 
@@ -162,7 +165,7 @@ function ozgardimi(){
     data.append(`category`, document.querySelector("#category1").value)
     data.append(`tavsif`, document.querySelector("#op2").value)
     data.append(`description`, document.querySelector("#op3").value)
-    data.append(`filial_id`, document.querySelector("#op4").value)
+    data.append(`filial_id`, pageiD)
     data.append(`price`, document.querySelector("#op5").value)
 
        axios.put(`https://fre.abbas.uz/api/mutahasis/${edit1ID}`,data).then(res=>{
@@ -215,6 +218,7 @@ data.append(`price`, document.querySelector("#nim5").value)
 
 
 function boshqapage(id){
+
   axios.get(`https://fre.abbas.uz/api/mutahasis/`).then(res=>{
     console.log(res.data)  
     axios.get(`https://fre.abbas.uz/api/category/`).then(res1=>{
@@ -234,6 +238,16 @@ function boshqapage(id){
     })
   })
 
+
+
+  axios.get(`https://fre.abbas.uz/api/filyal_mark/`).then(res=>{
+    const Filter10=res.data.filter(item=>item.filyal_id==id)
+    setMark(Filter10)
+    console.log(Filter10,"Salom1");
+  })
+
+
+
   setPageiD(id)
 }
 function pageyana(id){
@@ -252,8 +266,8 @@ function pageyana(id){
 
 })
 
-
 }
+
 
 
 function tezoch(id){
@@ -350,7 +364,7 @@ function lala(){
 function lala1(){
   var data = new FormData;
   data.append(`image`, document.querySelector("#nnn5").value)
-  data.append(`filyal_id`, document.querySelector("#nnn6").value)
+  data.append(`filyal_id`, pageiD)
 
      axios.post(`https://fre.abbas.uz/api/filyal_image/`,data).then(res=>{
     alert("Успешно")
@@ -381,7 +395,7 @@ function filyaledit(id){
 function ozgard11(){
   var data = new FormData;
   data.append(`image`, document.querySelector("#fila").value)
-  data.append(`filyal_id`, document.querySelector("#fila1").value)
+  data.append(`filyal_id`, pageiD)
 
      axios.put(`https://fre.abbas.uz/api/filyal_image/${filyal2}`,data).then(res=>{
     alert("Успешно")
@@ -403,16 +417,15 @@ function ozgard11(){
         <table id="customers">
   <tr>
     <th>id</th>
-    <th>image</th>
-    <th>address</th>
+    <th>картинки</th>
+    <th>адрес</th>
     <th>location</th>
     <th>longuage</th>
-    <th>name</th>
-    <th>desc</th>
-    <th>phone</th>
+    <th>Имя</th>
+    <th>телефон</th>
     <th>creator</th>
-    <th>delete</th>
-    <th>edit</th>
+    <th>удалить</th>
+    <th>редактировать</th>
   </tr>
   {data.map((item)=>{
     return(
@@ -424,11 +437,10 @@ function ozgard11(){
         <td  onClick={()=>boshqapage(item.id)}>{item.location}</td>
         <td  onClick={()=>boshqapage(item.id)}>{item.longuage}</td>
         <td  onClick={()=>boshqapage(item.id)}>{item.name}</td>
-        <td  onClick={()=>boshqapage(item.id)}>{item.desc}</td>
         <td  onClick={()=>boshqapage(item.id)}>{item.phone}</td>
         <td  onClick={()=>boshqapage(item.id)}>{item.creator}</td>
-        <td><button onClick={()=>modalOpen(item.id)}>delete</button></td>
-        <td><button onClick={()=>edit1112(item.id)}>edit</button></td>
+        <td><button onClick={()=>modalOpen(item.id)}>удалить</button></td>
+        <td><button onClick={()=>edit1112(item.id)}>редактировать</button></td>
   </tr>
       </>
     )
@@ -491,9 +503,9 @@ function ozgard11(){
       <input type="url" id='oz' /> <br />
       <span>Добавить адрес</span> <br />
       <input type="text" id='oz1' /><br />
-      <span>Добавить локацию</span><br />
+      <span>location</span><br />
       <input type="number" id='oz2' /><br />
-      <span>Добавить язык</span><br />
+      <span>longuage</span><br />
       <input type="number" id='oz3' /><br />
       <span>Добавить название</span><br />
       <input type="text" id='oz4' /><br />
@@ -501,7 +513,7 @@ function ozgard11(){
       <input type="text" id='oz5' /><br />
       <span>Добавить номер</span><br />
       <input type="number" id='oz6' /><br />
-      <span>Добавить марку</span><br />
+      <span>Добавить creator</span><br />
       <input type="number" id='oz7' /><br />
       <span>min_time</span><br />
       <input type="number" id='oz8' /><br />
@@ -511,21 +523,29 @@ function ozgard11(){
 </div></div> ):(page===2?(
   <div>
 
+
 <div className="nazad-btn">
   <button onClick={()=>setPage(1)}>назад</button>
 </div>
   <div className="bt">
             <button onClick={()=>qoshush()}>Добавить</button>
         </div>
+
+
+
+
+
+
+        <p>Тип места</p>
          <table id="customers">
   <tr>
     <th>id</th>
-    <th>category</th>
+    <th>Специализация</th>
     <th>tavsif</th>
     <th>description</th>
-    <th>price</th>
-    <th>delete</th>
-    <th>edit</th>
+    <th>Цена в час</th>
+    <th>удалить</th>
+    <th>редактировать</th>
   </tr>
   {data1.map((item)=>{
     return(
@@ -536,8 +556,8 @@ function ozgard11(){
             <td onClick={()=>pageyana(item.id)}>{item.tavsif}</td>
             <td onClick={()=>pageyana(item.id)}>{item.description}</td>
             <td onClick={()=>pageyana(item.id)}>{item.price}</td>
-            <td><button onClick={()=>min(item.id)}>delete</button></td>
-            <td><button onClick={()=>ozgartir(item.id)}>edit</button></td>
+            <td><button onClick={()=>min(item.id)}>удалить</button></td>
+            <td><button onClick={()=>ozgartir(item.id)}>редактировать</button></td>
         </tr>
         </>
     )
@@ -547,14 +567,13 @@ function ozgard11(){
 <div className="all-btn">
   <button onClick={()=>document.querySelector(".bu-filyal-omagad-2").style=`display:block`}>dabavit</button>
 </div>
-
+<p>Filyal картинки </p>
 <table id="customers">
   <tr>
     <th>id</th>
-    <th>image</th>
-    <th>filyal_id</th>
-    <th>edit</th>
-    <th>delete</th>
+    <th>картинки</th>
+    <th>редактировать</th>
+    <th>удалить</th>
   </tr>
   {filyal.map((item)=>{
     return(
@@ -562,9 +581,8 @@ function ozgard11(){
       <tr>
         <td>{item.id}</td>
         <td  className='img'><img src={item.image} alt="" /></td>
-        <td>{item.filyal_id}</td>
-        <td><button onClick={()=>filyaledit(item.id)}>edit</button></td>
-        <td><button onClick={()=>filyaldelete(item.id)}>delete</button></td>
+        <td><button onClick={()=>filyaledit(item.id)}>редактировать</button></td>
+        <td><button onClick={()=>filyaldelete(item.id)}>удалить</button></td>
       </tr>
       
       </>
@@ -574,7 +592,31 @@ function ozgard11(){
     
 </table>
 
-
+<table id="customers">
+  <tr>
+    <th>id</th>
+    <th>text</th>
+    <th>creator</th>
+    <th>time_create</th>
+    <th>редактировать</th>
+    <th>удалить</th>
+  </tr>
+  {mark.map((item)=>{
+    return(
+      <>
+      <tr>
+        <td>{item.mark}</td>
+        <td>{item.text}</td>
+        <td>{item.creator}</td>
+        <td>{item.time_create}</td>
+        <td><button>редактировать</button></td>
+        <td><button>удалить</button></td>
+     </tr>
+      </>
+    )
+  })}
+    
+</table>
 
 <div className="bu-filyal-42">
 
@@ -585,10 +627,8 @@ function ozgard11(){
   X 
 </div>
 </div>
-        <span>image</span><br />
+        <span>картинки</span><br />
         <input type="text" id='fila' /><br />
-        <span>filyal_id</span><br />
-        <input type="number" id='fila1' /><br />
         <button onClick={()=>ozgard11()} >изменить</button>
     </div>
 </div>
@@ -618,9 +658,7 @@ function ozgard11(){
       <input type="text" id='op2' /><br />
       <span>description</span><br />
       <input type="text" id='op3' /><br />
-      <span>filial_id</span><br />
-      <input type="number" id='op4' /><br />
-      <span>price</span><br />
+      <span>цена</span><br />
       <input type="number" id='op5' /><br />
       <button onClick={()=>ozgardimi()}>edit</button>
     </div>
@@ -683,7 +721,7 @@ function ozgard11(){
 <input type="text" id='nim2' /> <br />
 <span>description</span> <br />
 <input type="text" id='nim3' /> <br />
-<span>price</span> <br />
+<span>цена</span> <br />
 <input type="number" id='nim5' /> <br />
 <button onClick={()=>qoshisihss()}>qoshish</button>
     </div>
@@ -705,10 +743,10 @@ function ozgard11(){
 <table id="customers">
   <tr>
     <th>id</th>
-    <th>time</th>
+    <th>время</th>
     <th>mutahasis_id</th>
-    <th>delete</th>
-    <th>edit</th>
+    <th>удалить</th>
+    <th>редактировать</th>
   </tr>
 {data4.map((item)=>{
     return(
@@ -717,8 +755,8 @@ function ozgard11(){
             <td>{item.id}</td>
             <td>{item.time}</td>
             <td>{item.mutahasis_id}</td>
-            <td><button onClick={()=>tezoch(item.id)}>delete</button></td>
-            <td><button onClick={()=>edit(item.id)}>edit</button></td>
+            <td><button onClick={()=>tezoch(item.id)}>удалить</button></td>
+            <td><button onClick={()=>edit(item.id)}>редактировать</button></td>
         </tr> 
         </>
     )
@@ -728,7 +766,7 @@ function ozgard11(){
 
 
 <div className="all-btn" style={{display:"flex",justifyContent:"end",marginBottom:"20px"}}>
-  <button onClick={()=>document.querySelector(".bu-filyal-omagad").style=`display:block`}>dabavit</button>
+  <button onClick={()=>document.querySelector(".bu-filyal-omagad").style=`display:block`}>добавлять</button>
 </div>
 
 <table id="customers">
@@ -736,8 +774,8 @@ function ozgard11(){
     <th>id</th>
     <th>image_url</th>
     <th>mutahasis_id</th>
-    <th>delete</th>
-    <th>edit</th>
+    <th>удалить</th>
+    <th>редактировать</th>
   </tr>
 
 {mutahasis.map((item)=>{
@@ -747,8 +785,8 @@ function ozgard11(){
           <td>{item.id}</td>
           <td className='img'><img src={item.image} alt="" /></td>
           <td>{item.mutahasis_id}</td>
-          <td><button onClick={()=>deletemutahasis(item.id)}>delete</button></td>
-          <td><button onClick={()=>setMutahasi12(item.id)}>edit</button></td>
+          <td><button onClick={()=>deletemutahasis(item.id)}>удалить</button></td>
+          <td><button onClick={()=>setMutahasi12(item.id)}>редактировать</button></td>
           
       </tr> 
     </>
@@ -785,7 +823,7 @@ function ozgard11(){
   X 
 </div>
 </div>
-        <span>time</span><br />
+        <span>время</span><br />
         <input type="time" id='fil' /><br />
         <span>mutahasis_id</span><br />
         <input type="text" id='fil1' /><br />
@@ -804,11 +842,11 @@ function ozgard11(){
   X 
 </div>
 </div>
-        <span>time</span><br />
+        <span>время</span><br />
         <input type="time" id='filp' /><br />
         <span>mutahasis_id</span><br />
         <input type="text" id='filp1' /><br />
-        <button onClick={()=>dabavit()} >dabavit</button>
+        <button onClick={()=>dabavit()} >добавлять</button>
     </div>
 </div>
 </div>
@@ -843,11 +881,11 @@ function ozgard11(){
   X 
 </div>
 </div>
-        <span>image_url</span><br />
+        <span>картинки</span><br />
         <input type="text" id='nnn' /><br />
         <span>mutahasis_id</span><br />
         <input type="text" id='nnn2' /><br />
-        <button onClick={()=>qoshishdwe()} >dabavit</button>
+        <button onClick={()=>qoshishdwe()} >добавлять</button>
     </div>
 </div>
 </div>
@@ -862,11 +900,11 @@ function ozgard11(){
   X 
 </div>
 </div>
-        <span>image_url</span><br />
+        <span>картинки</span><br />
         <input type="text" id='nnn3' /><br />
         <span>mutahasis_id</span><br />
         <input type="text" id='nnn4' /><br />
-        <button onClick={()=>lala()} >dabavit</button>
+        <button onClick={()=>lala()} >добавлять</button>
     </div>
 </div>
 </div>
@@ -881,11 +919,9 @@ function ozgard11(){
   X 
 </div>
 </div>
-<span>image</span><br />
-        <input type="text" id='nnn5' /><br />
-        <span>filyal_id</span><br />
-        <input type="number" id='nnn6' /><br />
-        <button onClick={()=>lala1()} >dabavit</button>
+<span>картинки</span><br />
+        <input type="text" id='nnn5' /><br /> 
+        <button onClick={()=>lala1()} >добавлять</button>
     </div>
 </div>
 </div>
